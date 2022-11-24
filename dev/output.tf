9627c4c0939205251641150f349ca22ed707d62c
thn28p <rmtmrcd20200915@gmail.com>
2022-11-25 01:47:49 +0800

    o 25 147

--- dev/output.tf
+++ dev/output.tf
@@ -1,10 +1,13 @@
 output "server_ip" {
-  value = digitalocean_droplet.tag7assign01.*.ipv4_address
+#   value = digitalocean_droplet.tag7assign01.*.ipv4_address
+#  }
+  value = digitalocean_droplet.*.*.ipv4_address
  }
 
 output "vpc_id" {
   description = "ID of project VPC"
   value       = digitalocean_vpc.web_vpc.id
+}
 
 output "lb_url" {
   description = "URL of load balancer"
